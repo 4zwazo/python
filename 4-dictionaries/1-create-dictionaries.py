bd0143f2992b674de6fe3864e96664082f5fac6c
pascallaurent <4zwazo@gmail.com>
2020-12-21 06:28:45 -0500

    creating dictionaries

--- 4-dictionaries/1-create-dictionaries.py
+++ 4-dictionaries/1-create-dictionaries.py
@@ -1,3 +1,13 @@
+# create an empty dict using dict constructor
+my_dict = dict()
+print(type(my_dict))
+
+# a list
+kv_list = [['key-1', 'value-1'], ['key-2', 'value-2']]
+# convert list to dict
+print(dict(kv_list))
+
+# create a list using curly braces
 a_emp_dict = {
   'first_name': 'jude',
   'last_name': 'ti cool',
@@ -6,6 +16,7 @@ a_emp_dict = {
 
 print(a_emp_dict)
 
+#
 a_emp_dict = dict(
   first_name='jude',
   last_name='ti cool',
