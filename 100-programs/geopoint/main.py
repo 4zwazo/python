b8d33ea645f97f33f86636be184205efca574b35
Pascal Laurent <4zwazo@gmail.com>
2021-01-26 22:28:33 -0500

    removing if condition

--- 100-programs/geopoint/main.py
+++ 100-programs/geopoint/main.py
@@ -1,19 +1,12 @@
 from folium import Map
 from geo import Geopoint
 
+# get values
 latitude = 18.6390
 longitude = 74.1181
 
 antipode_latitude = latitude * -1
 antipode_longitude = 0
-
-if longitude <= 0:
-    antipode_longitude = longitude * 180
-elif longitude == 0:
-    antipode_longitude = 180
-
-print(antipode_longitude)
-location = [antipode_latitude, antipode_longitude]
 
 jmap = Map(location)
 jmap.save(str('antipode.html'))
