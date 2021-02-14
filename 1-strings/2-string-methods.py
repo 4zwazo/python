febd796933c95ab5ab249660aef6cd6353c8b1d6
Pascal Laurent <4zwazo@gmail.com>
2021-02-14 06:12:27 -0500

    upper and split functions

--- 1-strings/2-string-methods.py
+++ 1-strings/2-string-methods.py
@@ -1,12 +1,13 @@
 # In python everything is an object
 # Strings have methods you can call on them
 
-name = 'pascal'
+name = 'pascal laurent'
 name.capitalize()
 
-print(name.capitalize()) #=> Pascal
-print(name.upper()) #=> PASCAL
-print(name.lower()) #=> pascal
+print(name.capitalize()) #=> Pascal laurent
+print(name.title()) #=> Pascal laurent
+print(name.upper()) #=> PASCAL LAURENT
+print(name.lower()) #=> pascal laurent
 
 print(dir(name)) # listing of methods and attributes you can call on strings
 
@@ -16,9 +17,15 @@ print(f'|{name}|')
 print(f'|{name.strip()}|')
 
 # cut the string into separate words, produces a list of words
-text = 'This is so cool to learn Python'
+text = 'This.is.so.cool.to.learn.Python'
 print(text.split())
 
 # give the 2nd word in the list
-print(text.split()[1])
+print(text.split()[0])
+
+# ['This', 'is.so.cool.to.learn.Python']
+print(text.split('.', 1))
+
+# ['This', 'is', 'so.cool.to.learn.Python']
+print(text.split('.', 2))
 
