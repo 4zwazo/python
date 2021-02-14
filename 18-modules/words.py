0ea11bcdd21bd1977bc846ad72e49e8733d88cd7
Pascal Laurent <4zwazo@gmail.com>
2021-02-13 23:00:48 -0500

    adding functions words and display_words

--- 18-modules/words.py
+++ 18-modules/words.py
@@ -3,10 +3,14 @@ from urllib.request import urlopen
 story = urlopen('http://sixty-north.com/c/t.txt')
 story_words = []
 
-for line in story:
-    line_words = line.decode('utf-8').split()
-    for word in line_words:
-        story_words.append(word)
+def words():
+    for line in story:
+        line_words = line.decode('utf-8').split()
+        for word in line_words:
+            story_words.append(word)
 
-story.close()
-print(story_words)
+    story.close()
+
+def display_words():
+    for word in story_words:
+        print(word)
