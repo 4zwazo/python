39c884a2728b8932a1345196110f12a9839607c4
Pascal Laurent <4zwazo@gmail.com>
2021-02-15 07:25:33 -0500

    adding for loop to add more marker in the map

--- 100-programs/geopoint/main2.py
+++ 100-programs/geopoint/main2.py
@@ -1,28 +1,32 @@
 from folium import Map, Marker, Popup
 from geo import Geopoint
 
-# get values
-latitude = 18.6390
-longitude = -74.1181
+# Location coordinates
+locations = [[18.6390, -74.1181],
+             [18.598334, -73.960801],
+             [18.608997, -74.384686],
+             [18.549278, -74.271252],
+             [18.19922, -73.75169]]
 
 # Folium Map instance
-mymap = Map(location = [latitude, longitude])
+mymap = Map(location=[18.6390, -74.1181])
 
 # house the content for the popup
 popup_content = ""
 
-# create a geo point instance
-geopoint = Geopoint(latitude=latitude, longitude=longitude)
+for loc in locations:
+    # create a geo point instance
+    geopoint = Geopoint(latitude=loc[0], longitude=loc[1])
 
-for weather in geopoint.get_weather():
-    hr = '<hr style="margin: 1px;">'
-    text = f'{weather[0][11:13]}h: {round(weather[1])}°F <img src="http://openweathermap.org/img/wn/{weather[3]}@2x.png" width="35">'
-    popup_content += text + hr
+    for weather in geopoint.get_weather():
+        hr = '<hr style="margin: 1px;">'
+        text = f'{weather[0][11:13]}h: {round(weather[1])}°F <img src="http://openweathermap.org/img/wn/{weather[3]}@2x.png" width="35">'
+        popup_content += text + hr
 
-print(popup_content)
-popup = Popup(popup_content, max_width=300)
-popup.add_to(geopoint)
-geopoint.add_to(mymap)
+    popup = Popup(popup_content, max_width=300)
+    popup.add_to(geopoint)
+    geopoint.add_to(mymap)
+    popup_content = ""
 
 # add a marker to the map
 # jeremie = Marker(location = [latitude, longitude], popup = 'Jeremie')
@@ -30,5 +34,3 @@ geopoint.add_to(mymap)
 
 # save map instance
 mymap.save('map.html')
-
-print(geopoint.get_weather()[0][0][11:13])
