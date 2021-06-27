e0352571f8e8e0d03c53ee1dcd9cf3efe367765d
Pascal Laurent <4zwazo@gmail.com>
2021-06-27 16:04:43 -0400

    working with default parameters

---
+++ 14-functions/2-default-parameter.py
@@ -0,0 +1,6 @@
+def greet_with(name='Jude', location='Boston'):
+  print(f'Hello {name}')
+  print(f'What is it like in {location}?')
+
+# keyword arguments
+greet_with( location='New York', name='Jude')
